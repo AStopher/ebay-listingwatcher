9e535f3ec53af45379bf85a94028eb9f01b41a68
alex@s5s.gg <Alex S>
2021-03-26 17:14:55 +0000

    - Add eBay API.

---
+++ eBayWatcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eBayWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
